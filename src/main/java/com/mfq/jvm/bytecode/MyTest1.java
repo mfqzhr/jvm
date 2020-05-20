281f79cc4f451d209cb3eac54be052bad2190ce5
mfqzhr <a584084434@outlook.com>
2020-05-20 14:53:39 +0800

    字节码

---
+++ src/main/java/com/mfq/jvm/bytecode/MyTest1.java
@@ -0,0 +1,27 @@
+package com.mfq.jvm.bytecode;
+
+/**
+ * @author ：穆繁强
+ * @date ：Created in 2020/5/14 22:12
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+public class MyTest1 {
+    private int a = 1;
+    MyTest1 myTest1 = new MyTest1();
+
+    public int getA() {
+
+        return a;
+    }
+
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
